4da1a0cc9f032dd05a69fd09ad9a10a9ec1bf974
Bonkesam <bonkesam9@gmail.com>
2025-06-29 15:30:03 +0300

    corrects the auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { getServerSession } from "next-auth/next"
 import { redirect } from "next/navigation"
+import { authOptions } from "./authOptions"
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions)
